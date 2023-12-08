d0bc3fd64fdad092a5690c6837d6d6d9efe2fa53
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-08 12:27:47 +0500

    2-mavzu

---
+++ C#Basic/Operatorlar/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Project
+{
+    public static void Main()
+    {
+        string name = "Sardor";
+
+        var a = name switch
+        {
+            "Sardor" => 4,
+            "Sarvar" => 2,
+        };
+
+        /*//Darajaga oshirish
+        Console.WriteLine(Math.Pow(2, 10));
+
+        //Ildiz
+        Console.WriteLine(Math.Sqrt(625));
+
+        //Butun
+        Console.WriteLine(Math.Floor(3.56));
+
+        //Ceil
+        Console.WriteLine(Math.Ceiling(9.81));
+
+        //Fail
+        Console.WriteLine(Math.Floor(-43.7));
+
+        //sin
+        Console.WriteLine(Math.Sin(0));
+
+        //cos
+        Console.WriteLine(Math.Cos(0));
+
+        //Equels
+        Console.WriteLine(Math.Equals("S","33"));
+
+        Console.WriteLine(Math.Log2(9));
+
+        //
+        Console.WriteLine(Math.ILogB(17));*/
+
+    }
+}
