8ae239b8fa80b2af3b4dccc914061c63935e6adb
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-28 13:32:04 +0500

    Time

--- C#Advanced/TIME/Program.cs
+++ C#Advanced/TIME/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*DateTime dateTime = DateTime.Now;
 
+Console.WriteLine(dateTime.ToUniversalTime());*/
+
+/*DateTime birthDay = new DateTime(2003, 2, 20, 0, 0, 0);
+
+Console.WriteLine((DateTime.Now - birthDay).Days / 365);*/
+
+/*DateTime dateTime = DateTime.Parse("20 Feb, 2003");
+
+Console.WriteLine(dateTime.Year);
+Console.WriteLine(dateTime.Month);
+Console.WriteLine(dateTime.Day);
+Console.WriteLine(dateTime.DayOfYear);
+Console.WriteLine(dateTime.DayOfWeek);*/
+
+/*Console.WriteLine(DateTime.SpecifyKind(dateTime, DateTimeKind.Local));*/
+
+
+/*Console.WriteLine(DateTime.Now.ToString("yyyy,MM-dd"));*/
+
+
+//culture
+/*var culture = Thread.CurrentThread.CurrentCulture;
+
+Console.WriteLine(culture);*/
+
+
+
+using System.Globalization;
+
+var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+foreach (var c in cultures)
+    Console.WriteLine(c);
+
+foreach (var c in cultures)
+    Console.WriteLine(c.DisplayName);
