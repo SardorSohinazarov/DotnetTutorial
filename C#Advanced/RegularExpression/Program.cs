fc73acf00ce3df9ffe964ed7c6e46a040df63280
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-04 13:50:19 +0500

    Regex

---
+++ C#Advanced/RegularExpression/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+
+Regex regex = new Regex("^abc$");
+
+Console.WriteLine(regex.IsMatch("abc"));
+Console.WriteLine(regex.IsMatch("abcde"));
+Console.WriteLine(regex.IsMatch("abcdefg"));
