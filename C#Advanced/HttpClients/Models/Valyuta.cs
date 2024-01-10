0901bfbe6757138b3e59c81dc2fd8e335ee29611
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-11 03:51:52 +0500

    HttpClientLar bilan ishlash

---
+++ C#Advanced/HttpClients/Models/Valyuta.cs
@@ -0,0 +1,12 @@
+﻿namespace HttpClients.Models
+{
+    public class Valyuta
+    {
+        public string title { get; set; }
+        public string code { get; set; }
+        public string cb_price { get; set; }
+        public string nbu_buy_price { get; set; }
+        public string nbu_cell_price { get; set; }
+        public string date { get; set; }
+    }
+}
---
+++ C#Advanced/HttpClients/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using HttpClients.Models;
+using System.Text.Json;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HttpClient httpClient = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, "https://nbu.uz/exchange-rates/json");
+
+        var response = httpClient.SendAsync(request).Result;
+
+        var body = response.Content.ReadAsStringAsync().Result;
+
+        var courses = JsonSerializer.Deserialize<List<Valyuta>>(body);
+
+        foreach (var c in courses)
+        {
+            Console.WriteLine(c.title);
+        }
+    }
+}
