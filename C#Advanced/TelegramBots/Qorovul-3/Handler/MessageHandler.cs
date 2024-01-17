6a52ce48a57cb7eea326d677aecd8784731b9b0c
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-17 13:08:33 +0500

    Kanal uchun QorovulBot

---
+++ C#Advanced/TelegramBots/Qorovul-3/Handler/MessageHandler.cs
@@ -0,0 +1,82 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Qorovul_3;
+
+public partial class Program
+{
+    private static async Task HandleMessageAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        var handler = update.Message.Type switch
+        {
+            MessageType.Text => HandleCommandsAsync(botClient, update, cancellationToken),
+            _ => HandleUnknownMessageTypeAsync(botClient, update, cancellationToken)
+        };
+
+        try
+        {
+            await handler;
+        }
+        catch (Exception ex) { }
+    }
+
+    private static async Task HandleUnknownMessageTypeAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static async Task HandleCommandsAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        var handler = update.Message.Text switch
+        {
+            "/start" => HandleStartCommandAsync(botClient, update, cancellationToken),
+            "/salom" => HandleSalomCommandAsync(botClient, update, cancellationToken),
+            _ => HandleUnknownCommandAsync(botClient, update, cancellationToken)
+        };
+
+        try
+        {
+            await handler;
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    private static async Task HandleUnknownCommandAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static async Task HandleSalomCommandAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        foreach (var id in ids)
+        {
+            await botClient.SendTextMessageAsync(
+               chatId: id,
+               text: "Salomat",
+               cancellationToken: cancellationToken);
+        }
+    }
+
+    private static async Task HandleStartCommandAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        await botClient.SendTextMessageAsync(
+            chatId: update.Message.Chat.Id,
+            text: "Qales endi",
+            cancellationToken: cancellationToken);
+    }
+
+    private static async Task HandleUnknownUpdateTypeAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static async Task HandleMyChatMemberAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        ids.Add(update.MyChatMember.Chat.Id);
+    }
+
+}
---
+++ C#Advanced/TelegramBots/Qorovul-3/Program.cs
@@ -0,0 +1,79 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Qorovul_3;
+
+public partial class Program
+{
+    public static List<long> ids = new List<long>();
+    private static async Task Main(string[] args)
+    {
+        var botClient = new TelegramBotClient("6886129392:AAFx9QxWG9TRF3yJoRrnwOg4v6aiat-Rris");
+
+        using CancellationTokenSource cts = new();
+
+        // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+        ReceiverOptions receiverOptions = new()
+        {
+            AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+        };
+
+        botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            pollingErrorHandler: HandlePollingErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: cts.Token
+        );
+
+        var me = await botClient.GetMeAsync();
+
+        Console.WriteLine($"Start listening for @{me.Username}");
+        Console.ReadLine();
+
+        // Send cancellation request to stop bot
+        cts.Cancel();
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+
+            //IsChannelMember()
+            var handler = update.Type switch
+            {
+                UpdateType.MyChatMember => HandleMyChatMemberAsync(botClient, update, cancellationToken),
+                UpdateType.ChannelPost => HandleChannelPostAsync(botClient, update, cancellationToken),
+                UpdateType.Message => HandleMessageAsync(botClient, update, cancellationToken),
+                _ => HandleUnknownUpdateTypeAsync(botClient, update, cancellationToken)
+            };
+            try
+            {
+                await handler;
+            }
+            catch (Exception ex) { }
+        }
+
+        Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+
+    private static async Task HandleChannelPostAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        await botClient.SendTextMessageAsync(
+         chatId: update.ChannelPost.Chat.Id,
+         text: "Zo'r post ekan",
+         replyToMessageId: update.ChannelPost.MessageId,
+         cancellationToken: cancellationToken);
+    }
+}
