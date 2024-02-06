d2ca1455319a430c6ab39c3fe5cccd1a84873c62
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-06 06:16:19 +0500

    Update Implemented

--- Database/CRUD.ADO.NET/Program.cs
+++ Database/CRUD.ADO.NET/Program.cs
@@ -6,7 +6,9 @@ internal class Program
     private static void Main(string[] args)
     {
         //AddProduct("Olma", 10000);
-        GetProduct(1);
+        //GetProduct(1);
+        //GetProducts();
+        UpdateProduct(1, "Nok", 5000);
     }
 
     private static void AddProduct(string name, int price)
@@ -42,6 +44,42 @@ internal class Program
             {
                 Console.WriteLine($"{reader["id"]} | {reader["name"].ToString().Trim()} , {reader["Price"]}");
             }
+        }
+    }
+
+    private static void GetProducts()
+    {
+        string query = "select * from Products";
+
+        using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+        {
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+            sqlCommand.Connection.Open();
+            var reader = sqlCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader["id"]} | {reader["name"].ToString().Trim()} , {reader["Price"]}");
+            }
+        }
+    }
+
+    private static void UpdateProduct(int id, string name, int price)
+    {
+        string query = "Update Products SET Name=@name, Price=@price where id=@id";
+
+        using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+        {
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@id", id);
+            sqlCommand.Parameters.AddWithValue("@name", name);
+            sqlCommand.Parameters.AddWithValue("@price", price);
+
+            sqlCommand.Connection.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlCommand.Connection.Dispose();
         }
     }
 }
