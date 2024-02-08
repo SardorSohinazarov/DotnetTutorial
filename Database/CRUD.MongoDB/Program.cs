04fc81fb05b1e39054acbc2abc80a0e4c5143901
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-09 03:26:20 +0500

    Insert

---
+++ Database/CRUD.MongoDB/Program.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string connectionString = "mongodb://localhost:27017";
+
+        MongoClient mongoDB = new MongoClient(connectionString);
+
+        var database = mongoDB.GetDatabase("CrudMongo");
+
+        var collection = database.GetCollection<Product>("Products");
+
+        Console.WriteLine(collection.Count(_ => true));
+
+        collection.InsertOne(new Product()
+        {
+            Name = "Telefon",
+            Description = "Yaxshi telefon",
+            Price = 93483.43423M
+        });
+
+        Console.WriteLine(collection.Count(_ => true));
+    }
+}
